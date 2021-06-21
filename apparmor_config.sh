81d0163d7d1159670650fc7bb2b9b2f722d79807
leone <791960492@qq.com>
2021-06-21 22:32:57 +0800

    init

--- apparmor_config.sh
+++ apparmor_config.sh
@@ -1,4 +1,4 @@
 # allow libvirt access /usr/local/bin
-sudo touch /etc/apparmor.d/local/usr.sbin.libvirtd && \
+sudo touch /etc/apparmor.d/local/usr.sbin.libvirtd || exit
 sudo sh -c "cat /etc/apparmor.d/local/usr.sbin.libvirtd | grep -Fxq '/usr/local/bin/* PUx,' || echo '/usr/local/bin/* PUx,' >> /etc/apparmor.d/local/usr.sbin.libvirtd"
 sudo apparmor_parser -r /etc/apparmor.d/usr.sbin.libvirtd
